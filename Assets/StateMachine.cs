8b8a9337a0668980babedc3093e6ec0b5c455042
Atakan Cinar <atakanccnar@outlook.com>
2023-07-17 00:42:43 +0300

    State machine setup

--- Assets/StateMachine.cs
+++ Assets/StateMachine.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateMachine : MonoBehaviour
+namespace StateStuff
 {
-    // Start is called before the first frame update
-    void Start()
+    public class StateMachine<T>
     {
-        
+
+        public State<T> currentState { get; private set; }
+        public T Owner; // "T", genel bir tür parametresini temsil eder,kullanıcıya özel veri tipini belirleme olanağı sağlar.
+
+        public StateMachine(T _o)
+        {
+            Owner = _o;
+            currentState = null;
+        }
+        public void ChangeState(State<T> _newState)
+        {
+            if (currentState != null)
+                currentState.ExitState(Owner);
+
+            currentState = _newState;
+            currentState.EnterState(Owner);
+        }
+        public void Update()
+        {
+            if (currentState != null)
+                currentState.UpdateState(Owner);
+        }
+
+    }
+    public abstract class State<T>
+    {
+        public abstract void EnterState(T _owner);
+        public abstract void ExitState(T _owner);
+        public abstract void UpdateState(T _owner);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
