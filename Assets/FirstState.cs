3278c70eb6d554e21edba09a725867c29a1ea0c4
Atakan Cinar <atakanccnar@outlook.com>
2023-07-17 00:58:06 +0300

    Setup firstState

--- Assets/FirstState.cs
+++ Assets/FirstState.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StateStuff; // namespace kullandık.
 
-public class FirstState : MonoBehaviour
+public class FirstState : State<AI>
 {
-    // Start is called before the first frame update
-    void Start()
+    private static FirstState instance;
+
+    private FirstState()
     {
-        
+        if (instance != null)
+        {
+            return; //Eğer varsa, yapıcı metot geri döner ve birden çok örneğin oluşmasını engeller.
+        }
+        instance = this;
+    }
+    public static FirstState Instance //FirstState sınıfının yalnızca bir örneğinin olmasını sağlar
+    {
+        get
+        {
+            if (instance == null)
+            {
+                new FirstState();
+            }
+            return instance; //Instance ilk kez çağrıldığında örnek oluşturulur ve sonraki çağrılarda aynı örnek döndürülür.
+
+        }
+    }
+    public override void EnterState(AI _owner)
+    {
+        Debug.Log("Entering first state");
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void ExitState(AI _owner)
     {
-        
+        Debug.Log("Exiting first state");
+
+    }
+
+    public override void UpdateState(AI _owner)
+    {
+
     }
 }
